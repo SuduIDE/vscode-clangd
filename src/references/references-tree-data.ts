65a7111903ab2d6321b9f91efef24b9a73470762
mprotasov <mikhail.protasov@gmail.com>
2023-04-07 17:03:57 +0300

    Change the way we show references in tree

--- src/references/references-tree-data.ts
+++ src/references/references-tree-data.ts
@@ -1,37 +1,55 @@
-import {ProviderResult, TreeDataProvider, TreeItem, TreeItemCollapsibleState, window, workspace} from "vscode";
+import {ProviderResult, TreeDataProvider, TreeItem, TreeItemCollapsibleState, Uri, workspace} from "vscode";
 import {LocationWithProfiles} from "./compilation-profiles";
 import {getPreviewChunks} from "./utils";
 
-export class ReferencesTreeData implements TreeDataProvider<string | LocationWithProfiles> {
-  private references: Map<string, LocationWithProfiles[]> = new Map();
+type FileInfo = {
+  uri: Uri,
+  profile: ProfileName
+};
+type ProfileName = string;
 
-  constructor(locationsWithProfiles: LocationWithProfiles[]) {
-    locationsWithProfiles.forEach(location => {
-      const uri = location.uri.toString()
-      const existing = this.references.get(uri) || [];
-      this.references.set(uri, [...existing, location]);
-    });
-  }
+export class ReferencesTreeData implements TreeDataProvider<ProfileName | FileInfo | LocationWithProfiles> {
+  constructor(private locationsWithProfiles: LocationWithProfiles[]) {}
 
 
-  getChildren(element?: string): ProviderResult<(string | LocationWithProfiles)[]> {
+  getChildren(element?: ProfileName | FileInfo): ProviderResult<(ProfileName | FileInfo | LocationWithProfiles)[]> {
     if (!element) {
-      return [...this.references.keys()];
+      // root, return all profiles
+      const profiles: string[] = [];
+      this.locationsWithProfiles.forEach(loc => {
+        profiles.push(...loc.profiles);
+      });
+
+      return [...new Set(profiles)];
     }
 
-    const locations = this.references.get(element);
-    if (locations) {
-      return locations;
+    if (typeof element === 'string') {
+      // profile, return files
+      return [
+        // get unique files
+        ...new Set(this.locationsWithProfiles
+          .filter(loc => loc.profiles.includes(element))
+          .map(loc => loc.uri.toString()))
+      ]
+        .map(u => ({uri: Uri.parse(u), profile: element}));
     }
 
-    return undefined;
+    // file, return locations
+    return this.locationsWithProfiles.filter(loc => loc.profiles.includes(element.profile) && loc.uri.toString() === element.uri.toString());
   }
 
-  getTreeItem(element: string | LocationWithProfiles): TreeItem | Thenable<TreeItem> {
+  getTreeItem(element: ProfileName | FileInfo | LocationWithProfiles): TreeItem | Thenable<TreeItem> {
     if (typeof element === 'string') {
-      return {collapsibleState: TreeItemCollapsibleState.Expanded, label: element.split('/').pop()};
+      // profile
+      return {collapsibleState: TreeItemCollapsibleState.Expanded, label: element};
     }
 
+    if (this.isFileInfo(element)) {
+      // file
+      return {collapsibleState: TreeItemCollapsibleState.Expanded, label: element.uri.path.split('/').pop()};
+    }
+
+    // location
     return workspace.openTextDocument(element.uri).then(textDocument => {
       const chunks = getPreviewChunks(textDocument, element.range);
 
@@ -41,7 +59,6 @@ export class ReferencesTreeData implements TreeDataProvider<string | LocationWit
           label: chunks.before + chunks.inside + chunks.after,
           highlights: [[chunks.before.length, chunks.before.length + chunks.inside.length]]
         },
-        description: element.profiles.join(', '),
         command: {
           title: 'Open',
           command: 'vscode.open',
@@ -49,8 +66,10 @@ export class ReferencesTreeData implements TreeDataProvider<string | LocationWit
         }
       };
     });
+  }
 
-
+  isFileInfo(value: any): value is FileInfo {
+    return 'uri' in value && 'profile' in value;
   }
 
 }
