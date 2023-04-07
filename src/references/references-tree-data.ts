29e1be44512039d5ed8e553ba7bf42f358085327
mprotasov <mikhail.protasov@gmail.com>
2023-04-07 18:11:31 +0300

    Show search item in references tree

--- src/references/references-tree-data.ts
+++ src/references/references-tree-data.ts
@@ -2,17 +2,20 @@ import {ProviderResult, TreeDataProvider, TreeItem, TreeItemCollapsibleState, Ur
 import {LocationWithProfiles} from "./compilation-profiles";
 import {getPreviewChunks} from "./utils";
 
+type SearchItem = {
+  item: string;
+}
 type FileInfo = {
   uri: Uri,
   profile: ProfileName
 };
 type ProfileName = string;
 
-export class ReferencesTreeData implements TreeDataProvider<ProfileName | FileInfo | LocationWithProfiles> {
-  constructor(private locationsWithProfiles: LocationWithProfiles[]) {}
+export class ReferencesTreeData implements TreeDataProvider<ProfileName | FileInfo | LocationWithProfiles | SearchItem> {
+  constructor(private locationsWithProfiles: LocationWithProfiles[], private searchItem: string) {
+  }
 
-
-  getChildren(element?: ProfileName | FileInfo): ProviderResult<(ProfileName | FileInfo | LocationWithProfiles)[]> {
+  getChildren(element?: ProfileName | FileInfo): ProviderResult<(ProfileName | FileInfo | LocationWithProfiles | SearchItem)[]> {
     if (!element) {
       // root, return all profiles
       const profiles: string[] = [];
@@ -20,7 +23,7 @@ export class ReferencesTreeData implements TreeDataProvider<ProfileName | FileIn
         profiles.push(...loc.profiles);
       });
 
-      return [...new Set(profiles)];
+      return [{item: this.searchItem}, ...new Set(profiles)];
     }
 
     if (typeof element === 'string') {
@@ -38,10 +41,20 @@ export class ReferencesTreeData implements TreeDataProvider<ProfileName | FileIn
     return this.locationsWithProfiles.filter(loc => loc.profiles.includes(element.profile) && loc.uri.toString() === element.uri.toString());
   }
 
-  getTreeItem(element: ProfileName | FileInfo | LocationWithProfiles): TreeItem | Thenable<TreeItem> {
+  getTreeItem(element: ProfileName | FileInfo | LocationWithProfiles | SearchItem): TreeItem | Thenable<TreeItem> {
     if (typeof element === 'string') {
       // profile
-      return {collapsibleState: TreeItemCollapsibleState.Expanded, label: element};
+      return {collapsibleState: TreeItemCollapsibleState.Expanded, label: `Profile: ${element}`};
+    }
+
+    if (this.isSearchItem(element)) {
+      const description = 'Looking for references: ';
+      return {collapsibleState: TreeItemCollapsibleState.None,
+        label: {
+          label: `${description}${element.item}`,
+          highlights: [[description.length, description.length + element.item.length]]
+        }
+      };
     }
 
     if (this.isFileInfo(element)) {
@@ -66,6 +79,10 @@ export class ReferencesTreeData implements TreeDataProvider<ProfileName | FileIn
         }
       };
     });
+  }
+
+  isSearchItem(value: any): value is SearchItem {
+    return 'item' in value;
   }
 
   isFileInfo(value: any): value is FileInfo {
